ad1e9e8c64e3e3800534e8c59fa431306478d87d
ibrahimcesar <email@ibrahimcesar.com>
2022-08-14 15:57:08 -0300

    🎉feat: removing custom headers

--- next.config.js
+++ next.config.js
@@ -1,40 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-
-const terryPratchett = {
-    key: 'X-Clacks-Overhead',
-    value: 'GNU Terry Pratchett',
-}
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
   images: {
     domains: ['raw.githubusercontent.com'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          terryPratchett
-        ],
-      },
-      {
-        source: '/:ditto',
-        headers: [
-          terryPratchett
-        ],
-      },
-      {
-        source: '/_next\/([^\/]+\/?)*',
-        headers: [
-          terryPratchett
-        ],
-      },
-    ]
-  },
+  }
 }
 
 module.exports = nextConfig
